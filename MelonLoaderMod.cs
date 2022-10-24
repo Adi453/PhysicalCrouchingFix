4293fa58ac887667a85916d11f3e5e8a7e6a38cb
Adi <Adi453@users.noreply.github.com>
2022-10-24 22:40:00 +0300

    Add project files.

---
+++ MelonLoaderMod.cs
@@ -0,0 +1,27 @@
+﻿using HarmonyLib;
+using MelonLoader;
+using SLZ.Data;
+using SLZ.Marrow.Utilities;
+using SLZ.Rig;
+
+namespace PhysicalCrouchingFix
+{
+    public static class BuildInfo
+    {
+        public const string Name = "PhysicalCrouchingFix"; // Name of the Mod.  (MUST BE SET)
+        public const string Author = "Adi"; // Author of the Mod.  (Set as null if none)
+        public const string Company = null; // Company that made the Mod.  (Set as null if none)
+        public const string Version = "1.0.0"; // Version of the Mod.  (MUST BE SET)
+        public const string DownloadLink = null; // Download Link for the Mod.  (Set as null if none)
+    }
+
+    public class PhysicalCrouchingFix : MelonMod
+    {
+        [HarmonyPatch(typeof(ControllerRig), nameof(ControllerRig.ApplyMovement))]
+        public static class RigPatch
+        {
+            public static void Prefix(ControllerRig __instance) => __instance._crouchLimit = (MarrowGame.xr.HMD != null && MarrowGame.xr.HMD.Position.y < DataManager.ActiveSave.PlayerSettings.PlayerHeight * 0.7f) ? 1f : __instance._crouchLimit;
+            public static void Postfix(ControllerRig __instance) => __instance._crouchLimit = (MarrowGame.xr.HMD != null && MarrowGame.xr.HMD.Position.y < DataManager.ActiveSave.PlayerSettings.PlayerHeight * 0.7f) ? 1f : __instance._crouchLimit;
+        }
+    }
+}
---
+++ Properties/AssemblyInfo.cs
@@ -0,0 +1,25 @@
+﻿using MelonLoader;
+using System.Reflection;
+using System.Resources;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle(PhysicalCrouchingFix.BuildInfo.Name)]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany(PhysicalCrouchingFix.BuildInfo.Company)]
+[assembly: AssemblyProduct(PhysicalCrouchingFix.BuildInfo.Name)]
+[assembly: AssemblyCopyright("Created by " + PhysicalCrouchingFix.BuildInfo.Author)]
+[assembly: AssemblyTrademark(PhysicalCrouchingFix.BuildInfo.Company)]
+[assembly: AssemblyCulture("")]
+[assembly: ComVisible(false)]
+//[assembly: Guid("")]
+[assembly: AssemblyVersion(PhysicalCrouchingFix.BuildInfo.Version)]
+[assembly: AssemblyFileVersion(PhysicalCrouchingFix.BuildInfo.Version)]
+[assembly: NeutralResourcesLanguage("en")]
+[assembly: MelonInfo(typeof(PhysicalCrouchingFix.PhysicalCrouchingFix), PhysicalCrouchingFix.BuildInfo.Name, PhysicalCrouchingFix.BuildInfo.Version, PhysicalCrouchingFix.BuildInfo.Author, PhysicalCrouchingFix.BuildInfo.DownloadLink)]
+
+
+// Create and Setup a MelonModGame to mark a Mod as Universal or Compatible with specific Games.
+// If no MelonModGameAttribute is found or any of the Values for any MelonModGame on the Mod is null or empty it will be assumed the Mod is Universal.
+// Values for MelonModGame can be found in the Game's app.info file or printed at the top of every log directly beneath the Unity version.
+[assembly: MelonGame(null, null)]
