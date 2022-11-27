17dcf87ef39768b45457a5b5832545a46b04066b
Adi <Adi453@users.noreply.github.com>
2022-11-27 17:10:05 +0200

    Updated for patch 2

--- MelonLoaderMod.cs
+++ MelonLoaderMod.cs
@@ -3,6 +3,7 @@ using MelonLoader;
 using SLZ.Marrow.Utilities;
 using SLZ.Rig;
 using SLZ.SaveData;
+using System;
 
 namespace PhysicalCrouchingFix
 {
@@ -17,11 +18,26 @@ namespace PhysicalCrouchingFix
 
     public class PhysicalCrouchingFix : MelonMod
     {
+        public static bool error = false;
+        public static void ChangeCrouchLimit(ControllerRig controllerRig)
+        {
+            if (error)
+                return;
+            //prevents wacky shit if an update breaks the mod
+            try{
+                controllerRig._crouchLimit = (MarrowGame.xr.HMD != null && MarrowGame.xr.HMD.Position.y < DataManager.ActiveSave.PlayerSettings.PlayerHeight * 0.7f) ? 1f : controllerRig._crouchLimit;
+            }
+            catch(TypeLoadException e)
+            {
+                error = true;
+                MelonLogger.Error(e);
+            }
+        }
         [HarmonyPatch(typeof(ControllerRig), nameof(ControllerRig.ApplyMovement))]
         public static class RigPatch
         {
-            public static void Prefix(ControllerRig __instance) => __instance._crouchLimit = (MarrowGame.xr.HMD != null && MarrowGame.xr.HMD.Position.y < DataManager.ActiveSave.PlayerSettings.PlayerHeight * 0.7f) ? 1f : __instance._crouchLimit;
-            public static void Postfix(ControllerRig __instance) => __instance._crouchLimit = (MarrowGame.xr.HMD != null && MarrowGame.xr.HMD.Position.y < DataManager.ActiveSave.PlayerSettings.PlayerHeight * 0.7f) ? 1f : __instance._crouchLimit;
+            public static void Prefix(ControllerRig __instance) => ChangeCrouchLimit(__instance);
+            public static void Postfix(ControllerRig __instance) => ChangeCrouchLimit(__instance);
         }
     }
 }
